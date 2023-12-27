770045f904e59e7c29addaff0af137746f8cb905
Clay McLeod <clay.l.mcleod@gmail.com>
2023-12-27 00:10:37 -0600

    chore: improves examples in module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 //! For example, consider the following invocation of a fictional hashing algorithm that
 //! works in chunks of four (4) bytes at a time:
 //!
-//! ```compile_fail
+//! ```ignore
 //! let mut hasher = MyHasher::default();
 //!
 //! hasher.write(b"Hi,");
@@ -99,9 +99,25 @@
 //! To use this new implementation with the [`std::hash::Hasher`] interface, you can use
 //! the [`PartitionedHasher`] adapter like so:
 //!
-//! ```compile_fail
+//! ```
+//! use std::hash::Hasher as _;
+//! # #[derive(Debug, Default)]
+//! # pub struct Simple(u64);
+//! #
+//! # impl bufhash::partitioned::Hasher<8> for Simple {
+//! #     fn write(&mut self, bytes: &[u8; 8]) {
+//! #         let data = u64::from_le_bytes(*bytes);
+//! #         self.0 = self.0.wrapping_add(data);
+//! #     }
+//# !
+//! #     fn finish(&self, bytes: &[u8]) -> u64 {
+//! #         self.0 << bytes.len()
+//! #     }
+//! # }
+//!
 //! let mut hasher = bufhash::PartitionedHasher::new(Simple::default());
 //! hasher.write(b"Hello, world!");
+//!
 //! println!("Result: {:#X}", hasher.finish());
 //! ```
 //!
